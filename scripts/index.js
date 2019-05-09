dde59dc341bacedd4edeb7612b041693db924b3b
Reagan Davenport <reagandavenport@Reagans-MacBook-Air.local>
2019-05-09 14:42:43 -0400

    task 1-3

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,43 @@
-console.log(`There are ${characters.length} characters in the array.`);
+//Task 1 - Num of Characters with A in their first name
+// characterWithA = [];
+// characters.forEach(function(character){
+   
+//     // console.log(character.name[0])
+//     if (character.name[0] == 'A'){
+//         console.log("found an A");
+//         characterWithA.push(character)
+//     }
+    
+// })
+// console.log(characterWithA);
+//168
+
+//Task 2 - Num of Characters with Z in their first name
+// characterWithZ = [];
+// characters.forEach(function(character){
+   
+//     // console.log(character.name[0])
+//     if (character.name[0] == 'Z'){
+//         console.log("found an Z");
+//         characterWithZ.push(character)
+//     }
+    
+// })
+// console.log(characterWithZ);
+
+//8
+
+
+//Task 3 - How many characters are dead
+// deadCharacters = [];
+// characters.forEach(function(character){
+   
+//     // console.log(character.name[0])
+//     if (character.died !== ""){
+//         console.log("found a dead character");
+//         deadCharacters.push(character)
+//     }
+    
+// })
+// console.log(deadCharacters); 
+//553
